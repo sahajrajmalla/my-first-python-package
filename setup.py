9e3a40c64af504e58181f20df47b426bc477a6f2
Sahaj Raj Malla <mallasahajraj@gmail.com>
2021-11-04 06:36:42 +0545

    published

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup
 setup(
-    name = 'helloworld',
+    name = 'hello-sahaj-raj-malla',
     version = '0.0.1',
     description = 'Say Hello',
     py_modules = ['helloworld'],
     package_dir = {'': 'src'},
     
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Lnaguage :: Python :: 3.7",
-        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
-        "Operating System :: OS Independent",
-    ],
+    # classifiers=[
+    #     "Programming Language :: Python :: 3",
+    #     "Programming Language :: Python :: 3.6",
+    #     "Programming Lnaguage :: Python :: 3.7",
+    #     "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+    #     "Operating System :: OS Independent",
+    # ],
 
     install_requires = [
         "pandas",
